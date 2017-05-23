ade40a981c8e07b94bdc1774adb146b7ec0445bf
karolsitkowski <karol.sitkowski@gmail.com>
2017-05-23 19:56:27 +0200

    Working program

--- src/main/java/pl/codementors/training/DrawTree.java
+++ src/main/java/pl/codementors/training/DrawTree.java
@@ -1,10 +1,67 @@
 package pl.codementors.training;
 
-/**
- * Created by sit0 on 23.05.17.
- */
+import java.util.Scanner;
+
+
 public class DrawTree {
     public static void main(String[] args) {
-        
+
+        int heightOfTree = readInputFromUser();
+        //System.out.println(heightOfTree);
+        drawTree(heightOfTree);
+
+
+
+
     }
+
+    public static int readInputFromUser() {
+        int heightOfTree;
+
+        while(true) {
+            try {
+                Scanner userInput = new Scanner(System.in);
+                heightOfTree = userInput.nextInt();
+                break;
+
+            }
+            catch (java.util.InputMismatchException e) {
+            System.out.println("Wpisz liczbę nie literę");
+            }
+        }
+        return  heightOfTree;
+    }
+
+    public static void drawTree (int height){
+
+
+        int numberOfStars = 1;
+        int numberOfSpaces = height - 1;
+
+        for (int i = 1;i<=height;i++) {
+
+            printSpaces(numberOfSpaces);
+            printStars(numberOfStars);
+
+            System.out.println();
+            numberOfStars+=2;
+            numberOfSpaces-=1;
+        }
+
+
+
+    }
+
+    public static void printStars(int stars){
+
+        for(int i = 0; i <stars;i++)
+            System.out.print("*");
+
+
+    }
+    public static void printSpaces(int spaces){
+        for(int i = 0; i <spaces;i++)
+            System.out.print(" ");
+    }
+
 }
